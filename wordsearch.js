a082377d287af0bdecb5e660137c24d6e924920d
Sszyh <zhuyer@outlook.com>
2022-08-11 13:02:10 -0700

    pair coding with Bryce and Gene

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const createVerticalArrs = (arr) => {
+    const outMatrix = [];
+    
+    for (let i = 0; i < arr[0].length; i++) {
+      let outArr = [];
+      for (let j = 0; j < arr.length; j++) {
+        outArr.push(arr[j][i])
+      }
+      outMatrix.push(outArr);
     }
-}
-
+    return outMatrix;
+  };
+  
+  const wordSearch = (letters, word) => { 
+      if (letters.length === 0) {
+          return false;
+      }
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      for (l of horizontalJoin) {
+          if (l.includes(word)) return true
+      }
+      const transposeLettters = createVerticalArrs(letters);
+      const verticalsJoin = transposeLettters.map(ls => ls.join(''))
+      for (l of verticalsJoin) {
+          if (l.includes(word)) return true
+      }
+      return false;
+  }
+  
 module.exports = wordSearch
